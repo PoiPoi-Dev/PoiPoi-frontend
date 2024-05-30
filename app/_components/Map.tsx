803324db18f97e0d5ddf73676cdd0b174687a462
Musashi117 <32604297+Musashi117@users.noreply.github.com>
2024-05-30 18:34:19 +0900

    chore: remove comments

--- app/_components/Map.tsx
+++ app/_components/Map.tsx
@@ -2,7 +2,6 @@
 import { useEffect, useRef, useState } from "react"
 
 import maplibregl from "maplibre-gl";
-// import { Protocol } from "pmtiles";
 
 export default function Map ():JSX.Element {
   const mapContainer = useRef(null);
@@ -14,28 +13,6 @@ export default function Map ():JSX.Element {
 
   useEffect(() => {
     if (map.current) return;
-
-    // const protocol = new Protocol();
-    // maplibregl.addProtocol("pmtiles", protocol.tile);
-
-    // const styleSpecification:maplibregl.StyleSpecification = {
-    //   version: 8,
-    //   "sources": {
-    //     "protomaps": {
-    //       "type": "vector",
-    //       "url": `https://api.protomaps.com/styles/v2/light.json?key=${process.env.NEXT_PUBLIC_PROTOMAPS_API_KEY}`,
-    //     }
-    //   },
-    //   layers: [
-    //     {
-    //         "id": "coastline",
-    //         "source": "protomaps",
-    //         "source-layer": "countries",
-    //         "type": "line",
-    //         "paint": {"line-color": "#198EC8"}
-    //     }
-    //   ],
-    // };
 
     map.current = new maplibregl.Map({
       container: mapContainer.current as unknown as HTMLElement,
