5bc8382e3a1823de3b97b989aba580c2450717ea
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-06-02 18:29:11 +0900

    fix: delete max-w-lg that cause display error

--- app/_components/ui/dialog.tsx
+++ app/_components/ui/dialog.tsx
@@ -37,7 +37,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "h-screen w-screen max-w-lg flex justify-center items-center shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:rounded-lg",
+        "h-screen w-screen flex justify-center items-center shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:rounded-lg",
         className
       )}
     >
