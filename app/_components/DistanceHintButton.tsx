19babe545531ce07b88c8e8795c88927afb2d58f
Musashi117 <32604297+Musashi117@users.noreply.github.com>
2024-06-03 19:10:03 +0900

    feat: display closest pin in meters

--- app/_components/DistanceHintButton.tsx
+++ app/_components/DistanceHintButton.tsx
@@ -3,7 +3,6 @@ import { Button } from "./ui/button";
 import { Pin } from "../_utils/global";
 import {
   Coordinates,
-  DegreesToMeters,
   GetDistanceFromCoordinatesToMeters,
 } from "../_utils/coordinateMath";
 
@@ -11,11 +10,17 @@ interface DistanceHintButtonProps {
   pins: Pin[];
 }
 
+/**
+ * TODO: Manage Button states based on closest pin's search radius.
+ * This can done by constantly checking the user coordinates.
+ * Manage how frequent this call is made.
+**/
+
 function DistanceHintButton({
   pins,
 }: DistanceHintButtonProps): React.JSX.Element {
   const [trackingPin, setTrackingPin] = useState<Pin | null>(null);
-  const [distanceToPin, setDistanceToPin] = useState<number | null>(null);
+  const [distanceToPin, setDistanceToPin] = useState<number>(0);
 
   const handleDistanceHintOnClick = () => {
     console.log("Button clicked");
@@ -26,7 +31,7 @@ function DistanceHintButton({
           latitude: position.coords.latitude,
         };
         console.log(userCoordinates);
-        let shortestDistance: number = Math.max();
+        let shortestDistance: number = Number.MAX_SAFE_INTEGER;
         let pinToTrack: Pin | null = null;
 
         //Finds the closest pin
@@ -39,16 +44,14 @@ function DistanceHintButton({
             userCoordinates,
             pinCoordinates
           );
-          console.log(distance);
           if (distance < shortestDistance) {
+            console.log(distance, )
             shortestDistance = distance;
             pinToTrack = pin;
           }
         }
-
         setTrackingPin(pinToTrack);
-        const distanceInMeters = DegreesToMeters(shortestDistance);
-        setDistanceToPin(distanceInMeters);
+        setDistanceToPin(shortestDistance);
       },
       (error) => {
         console.error(error);
@@ -58,7 +61,7 @@ function DistanceHintButton({
   return (
     <div className="fixed right-0 bottom-10 w-auto h-auto">
       <Button onClick={handleDistanceHintOnClick}>
-        distance to POI: {distanceToPin}
+        distance to POI: {Math.trunc(distanceToPin)} m
       </Button>
     </div>
   );
