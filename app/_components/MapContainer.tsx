182cbacf921c1faa4682d90063a4f0bac20976a9
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-06-08 21:50:21 +0900

    feat: make Filter Button component, store filter array state in MapContainer

--- app/_components/MapContainer.tsx
+++ app/_components/MapContainer.tsx
@@ -11,7 +11,9 @@ import HintButton from "./HintButton";
 import { AuthContext } from "./useContext/AuthContext";
 import { getAuthService } from "@/config/firebaseconfig";
 import GameControls from "./GameControls";
-import { ConvertGeolocationPositionToCoordinates, Coordinates,
+import {
+  ConvertGeolocationPositionToCoordinates,
+  Coordinates,
   GetDistanceFromCoordinatesToMeters,
 } from "../_utils/coordinateMath";
 import useGeolocation from "../_hooks/useGeolocation";
@@ -119,48 +121,52 @@ function MapInner() {
     const distance = GetDistanceFromCoordinatesToMeters(userCoordinates, pinCoordinates);
     console.log("Calculate distance", distance );
     setDistanceToTrackingPin(distance);
-  }
-    
+  };
+
   /**
-  * Sets the user's coordinates
-  * @param position 
-  */
+   * Sets the user's coordinates
+   * @param position
+   */
   const handleSetUserCoordinates = (position: GeolocationPosition) => {
-    const userCoord:Coordinates = ConvertGeolocationPositionToCoordinates(position);
+    const userCoord: Coordinates =
+      ConvertGeolocationPositionToCoordinates(position);
     setUserCoordinates(userCoord);
-  }
-     
+  };
+
   /**
-  * Sets closestNotCompletedPin to the closes pin BY POSITION
-  * Currently does not account for filters
-  * @param position 
-  */
+   * Sets closestNotCompletedPin to the closes pin BY POSITION
+   * Currently does not account for filters
+   * @param position
+   */
   const handleSetClosestNotCompletedPin = (position: GeolocationPosition) => {
     const userCoordinates: Coordinates = {
       longitude: position.coords.longitude,
       latitude: position.coords.latitude,
     };
-  
+
     let shortestDistance: number = Number.MAX_SAFE_INTEGER;
     let closestPin: Pin | null = null;
-  
+
     for (const pin of poiData) {
       if (pin.is_completed) continue;
-   
+
       const pinCoordinates: Coordinates = {
         longitude: pin.exact_longitude,
         latitude: pin.exact_latitude,
       };
-    
-      const distance: number = GetDistanceFromCoordinatesToMeters(userCoordinates, pinCoordinates);
+
+      const distance: number = GetDistanceFromCoordinatesToMeters(
+        userCoordinates,
+        pinCoordinates
+      );
       if (distance < shortestDistance) {
         shortestDistance = distance;
         closestPin = pin;
       }
     }
     setClosestNotCompletedPin(closestPin);
-  }
-     
+  };
+
   useGeolocation(handleSetUserCoordinates);
   useGeolocation(handleSetClosestNotCompletedPin);
 
