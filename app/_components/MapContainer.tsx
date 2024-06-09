cbc14be524e22abee4dc4d967da18ea22e52e40d
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-06-09 09:42:54 +0900

    feat: display distance w/ 2 decimal, if exceed 1000 meter, display in kilometer instead

--- app/_components/MapContainer.tsx
+++ app/_components/MapContainer.tsx
@@ -269,6 +269,23 @@ function MapInner() {
             />
             <div className="absolute bottom-6 flex w-screen justify-center items-center">
               <Button onClick={() => setGuessPoiPosition(null)}>Next</Button>
+              <p>
+                distance:
+                {GetDistanceFromCoordinatesToMeters(
+                  userCoordinates,
+                  guessPoiPosition
+                ) > 1000
+                  ? (
+                      GetDistanceFromCoordinatesToMeters(
+                        userCoordinates,
+                        guessPoiPosition
+                      ) / 1000
+                    ).toFixed(2) + "km."
+                  : GetDistanceFromCoordinatesToMeters(
+                      userCoordinates,
+                      guessPoiPosition
+                    ).toFixed(2) + "m."}
+              </p>
             </div>
           </>
         )}
