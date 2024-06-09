496680c160e62b96f7eaaa2fb82a1e06cb3230ae
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-06-09 22:12:35 +0900

    fix: CSS bug that prevent drawer to overlay the map

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -3,10 +3,10 @@ import { AuthProvider } from "../_components/useContext/AuthContext";
 
 export default function Home(): JSX.Element {
   return (
-      <div className="absolute overflow-hidden inset-0 bg-mapBg">
-        <AuthProvider>
-          <MapContainer />
-        </AuthProvider>
-      </div>
+    <div className="absolute inset-0 bg-mapBg">
+      <AuthProvider>
+        <MapContainer />
+      </AuthProvider>
+    </div>
   );
 }
