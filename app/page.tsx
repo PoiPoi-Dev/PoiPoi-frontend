2732fe96008d02ff62faa261523d83c6e957ff71
Dominik Sakic <dominik.sakic@yahoo.com>
2024-06-09 11:12:15 +0900

    feat: scaffold Leaderboard structure

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,22 @@ export default function Home(): JSX.Element {
           Save your achievments!
         </p>
       </Link>
-      
+      <Link
+        href="/leaderboard"
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-slate-400 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h2 className="mb-3 text-2xl font-semibold">
+          Leaderboard{" "}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Scores of all the active Players!
+        </p>
+      </Link>
     </main>
   );
 }
