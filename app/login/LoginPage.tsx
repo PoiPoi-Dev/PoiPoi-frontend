e6d843c7a909799e325c9555747376d03840c4bc
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-06-12 11:50:01 +0900

    style: login page restyle with shadCN input, label

--- app/login/LoginPage.tsx
+++ app/login/LoginPage.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useState, useContext } from "react";
 import { User } from "../_utils/global";
-import { Button } from "../_components/ui/button";
 import {
   createUser,
   isVerified,
@@ -9,11 +8,21 @@ import {
   logoutUser,
 } from "../_actions/authenticationActions";
 import { getAuthService } from "@/config/firebaseconfig";
-import { AuthContext } from "../_components/useContext/AuthContext"
+import { AuthContext } from "../_components/useContext/AuthContext";
+import { Button } from "../_components/ui/button";
+import { Input } from "@/app/_components/ui/input";
+import { Label } from "@/app/_components/ui/label";
+import FooterMenu from "../_components/FooterMenu";
 
 const LoginPage: React.FC = () => {
   const [loginWindowStatus, setLoginWindowStatus] = useState<number>(0);
-  const [user, setUser] = useState<User>({} as User);
+  const [user, setUser] = useState<User>({
+    email: "",
+    creatingEmail: "",
+    password: "",
+    creatingPassword: "",
+    displayName: "",
+  } as User);
   const firebaseUser = useContext(AuthContext);
 
   useEffect(() => {
@@ -22,6 +31,10 @@ const LoginPage: React.FC = () => {
     }
     return () => {};
   }, [firebaseUser]);
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
 
   const handleInputChange = (e: {
     target: { name: string; value: string };
@@ -39,9 +52,13 @@ const LoginPage: React.FC = () => {
     e.preventDefault();
 
     try {
-      if (!user.email || !user.password || !user.displayName)
+      if (!user.creatingEmail || !user.creatingPassword || !user.displayName)
         throw "Invalid User/Password/Display Name";
-      await createUser(user.email, user.password, user.displayName);
+      await createUser(
+        user.creatingEmail,
+        user.creatingPassword,
+        user.displayName
+      );
     } catch (error) {
       console.error(error);
     }
@@ -51,7 +68,7 @@ const LoginPage: React.FC = () => {
     e: React.FormEvent<HTMLFormElement> | React.FormEvent<HTMLButtonElement>
   ) => {
     e.preventDefault();
-    
+
     try {
       if (!user.email || !user.password) throw "Invalid User/Password";
       await loginUser(user.email, user.password);
@@ -67,12 +84,15 @@ const LoginPage: React.FC = () => {
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   const checkIfVerified = async () => {
     try {
       const auth = await getAuthService();
-      if (!auth.currentUser) throw "No current user";
+      if (!auth.currentUser) {
+        alert("No current user");
+        throw "No current user";
+      }
       const idToken: string = await auth.currentUser.getIdToken(true);
       await isVerified(idToken);
     } catch (error) {
@@ -80,123 +100,171 @@ const LoginPage: React.FC = () => {
     }
   };
 
-  const renderSignOutLayout = ():React.JSX.Element => {
+  const renderSignOutLayout = (): React.JSX.Element => {
     return (
       <>
-        Would you like to sign out?
+        <h1 className="text-primary text-2xl font-bold">
+          Would you like to sign out?
+        </h1>
         <div>
-          <Button onClick={(): void => void handleLogout()}>Sign out</Button>
+          <Button className="w-full" onClick={(): void => void handleLogout()}>
+            Sign out
+          </Button>
         </div>
       </>
-    )
-  }
+    );
+  };
 
   const renderCreateAccountLayout = () => {
     return (
-      <>
-      Create new account:
+      <div className="w-full">
+        <h1 className="text-primary text-2xl font-bold">Create new account</h1>
         <form onSubmit={void handleCreateNewAccount}>
-          <div>
-            <label htmlFor="email">Email address:</label>
-            <input
-              className="border border-black rounded"
-              type="text"
-              required
-              onChange={handleInputChange}
-              name="email"
-              value={user.email}
-            ></input>
+          {/* INPUT */}
+          <div className="flex flex-col gap-4 mb-4">
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="creatingEmail">Email :</Label>
+              <Input
+                type="email"
+                id="creatingEmail"
+                required
+                placeholder="Email"
+                onChange={({ target: { value } }) =>
+                  handleInputChange({
+                    target: { name: "creatingEmail", value },
+                  })
+                }
+                value={user.creatingEmail}
+              />
+            </div>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="creatingUsername">Username :</Label>
+              <Input
+                type="username"
+                id="creatingUsername"
+                required
+                placeholder="username"
+                onChange={({ target: { value } }) =>
+                  handleInputChange({
+                    target: { name: "displayName", value },
+                  })
+                }
+                value={user.displayName}
+              />
+            </div>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="creatingPassword">Password :</Label>
+              <Input
+                type="password"
+                id="creatingPassword"
+                required
+                placeholder="password"
+                onChange={({ target: { value } }) =>
+                  handleInputChange({
+                    target: { name: "creatingPassword", value },
+                  })
+                }
+                value={user.creatingPassword}
+              />
+            </div>
           </div>
-          <div>
-            <label htmlFor="username">Set display name:</label>
-            <input
-              className="border border-black rounded"
-              type="text"
-              required
-              onChange={handleInputChange}
-              name="displayName"
-              value={user.displayName}
-            ></input>
+
+          {/* CTA */}
+          <div className="flex flex-col gap-4">
+            <Button onClick={(e) => void handleCreateNewAccount(e)}>
+              Create account
+            </Button>
           </div>
-          <div>
-            <label htmlFor="password">Set password:</label>
-            <input
-              className="border border-black rounded"
-              type="password"
-              required
-              onChange={handleInputChange}
-              name="password"
-              value={user.password}
-            ></input>
-          </div>
-          <Button onClick={(e) => void handleCreateNewAccount(e)}>
-            Create account
-          </Button>
-          <Button onClick={() => setLoginWindowStatus(1)}> Switch to Login Menu </Button>
         </form>
-      </>
-    )
-  }
+
+        <div>
+          <Button
+            variant={"outline"}
+            className="w-full mt-4"
+            onClick={() => setLoginWindowStatus(1)}
+          >
+            {" "}
+            Switch to Login Menu{" "}
+          </Button>
+        </div>
+      </div>
+    );
+  };
 
   const renderSignInWindow = () => {
     return (
-      <>
-        Log in:
-          <form onSubmit={(e) => void handleLogin(e)}>
-            {/* BEWARE! onSubmit event fires when user presses "enter" key at any point */}
-            <div>
-              <label htmlFor="email">Email address:</label>
-              <input
-                className="border border-black rounded"
-                type="text"
+      <div className="w-full">
+        <h1 className="text-primary text-2xl font-bold">Login</h1>
+
+        <form onSubmit={(e) => void handleLogin(e)}>
+          {/* BEWARE! onSubmit event fires when user presses "enter" key at any point */}
+          <div className="flex flex-col gap-4 mb-4">
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="email">Email :</Label>
+              <Input
+                type="email"
+                id="email"
                 required
-                onChange={handleInputChange}
-                name="email"
+                placeholder="Email"
+                onChange={({ target: { value } }) =>
+                  handleInputChange({ target: { name: "email", value } })
+                }
                 value={user.email}
-              ></input>
+              />
             </div>
-            <div>
-              <label htmlFor="password">Set password:</label>
-              <input
-                className="border border-black rounded"
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <Label htmlFor="password">Password :</Label>
+              <Input
                 type="password"
+                id="password"
                 required
-                onChange={handleInputChange}
-                name="password"
+                placeholder="password"
+                onChange={({ target: { value } }) =>
+                  handleInputChange({ target: { name: "password", value } })
+                }
                 value={user.password}
-              ></input>
+              />
             </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
             <Button onClick={(e) => void handleLogin(e)}>Log in</Button>
-            <Button onClick={() => setLoginWindowStatus(0)}>
-              {" "}
-              Switch to create new account menu
-            </Button>
-          </form>
-        </>
-    )
-  }
+          </div>
+        </form>
+
+        <Button
+          variant={"outline"}
+          className="w-full mt-4"
+          onClick={() => setLoginWindowStatus(0)}
+        >
+          {" "}
+          Switch to create new account menu
+        </Button>
+      </div>
+    );
+  };
 
   const renderLoginWindow = (status: number) => {
     return (
-      <div className="bg-white border rounded shadow-lg mt-2 p-2">
-        {
-          (status === 2) ? renderSignOutLayout()
-          : (status === 1) ?  renderSignInWindow()
-          : renderCreateAccountLayout()
-        }
+      <div className="bg-white border rounded-2xl shadow-lg mt-2 p-4 w-full animate-fade fade-in">
+        {status === 2
+          ? renderSignOutLayout()
+          : status === 1
+          ? renderSignInWindow()
+          : renderCreateAccountLayout()}
       </div>
-    )
-  }
+    );
+  };
 
   return (
-    <div className="flex flex-col items-center justify-between">
-      { renderLoginWindow(loginWindowStatus) }
-      {/* creation */}
+    <div className="flex flex-col items-center justify-center gap-4 w-screen h-screen px-4">
+      {renderLoginWindow(loginWindowStatus)}
       <div>
-        <Button onClick={() => void checkIfVerified()}>
+        <Button variant={"link"} onClick={() => void checkIfVerified()}>
           Click here to test if verified
         </Button>
       </div>
+      <FooterMenu variant="account" />
     </div>
   );
 };
