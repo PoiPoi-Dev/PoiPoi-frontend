ab6280e168e9648e1cc90ad4efb8bf8eea67ec8f
Deana Chou <130136996+deanachou@users.noreply.github.com>
2024-06-14 17:57:22 +0900

    CHORE: delete unnecessary div

--- app/how-to-play/page.tsx
+++ app/how-to-play/page.tsx
@@ -1,11 +1,5 @@
 import HowToPlay from "./HowToPlay";
 
-
 export default function Home(): JSX.Element {
-  return (
-    <div>
-      <HowToPlay />
-     
-    </div>
-  );
+  return <HowToPlay />;
 }
