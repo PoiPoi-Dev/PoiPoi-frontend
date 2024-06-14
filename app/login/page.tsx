4cb36960f14330987db87547e2943561c03ba8ae
Dominik Sakic <dominik.sakic@yahoo.com>
2024-06-14 20:32:11 +0900

    chore: refactor component to use CSR

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,9 @@
 import LoginPage from "./LoginPage";
-import { AuthProvider } from "../_components/useContext/AuthContext";
 
 export default function Home(): JSX.Element {
   return (
     <div className="absolute overflow-hidden inset-0 bg-gray-200">
-      <AuthProvider>
-        <LoginPage />
-      </AuthProvider>
+      <LoginPage />
     </div>
   );
 }
