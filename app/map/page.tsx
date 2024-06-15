4a19a04ed1ff6fd44099e5444c6eb402a01466f9
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-06-15 10:37:53 +0900

    fix: z-index bug and restyle to sub-menu

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,19 +1,18 @@
 import MapContainer from "../_components/MapContainer";
 import { AuthProvider } from "../_components/useContext/AuthContext";
 import Link from "next/link";
-import { BsQuestionDiamondFill } from "react-icons/bs";
-import { ButtonIconCircle } from "../_components/ui/MenuIconCircle";
-
+import { BsQuestionCircleFill } from "react-icons/bs";
 
 export default function Home(): JSX.Element {
   return (
     <div className="absolute inset-0 bg-mapBg">
       <AuthProvider>
         <MapContainer />
-        <Link className="fixed bottom-20 left-5" href="/how-to-play">
-          <ButtonIconCircle text="Tutorial">
-          <BsQuestionDiamondFill size={24}/>
-          </ButtonIconCircle>
+        <Link className="fixed top-56 right-2.5 z-[50]" href="/how-to-play">
+          <BsQuestionCircleFill
+            size={30}
+            className="text-white bg-primary rounded-full shadow-2xl"
+          />
         </Link>
       </AuthProvider>
     </div>
