ec4e724f1212ce1f908841c3011582508bee3415
Flumanuck <machlisj@gmail.com>
2024-06-16 12:36:28 +0900

    bugfix: reimplement authprovider on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,12 @@
 import LoginPage from "./LoginPage";
+import { AuthProvider } from "../_components/useContext/AuthContext";
 
 export default function Home(): JSX.Element {
   return (
     <div className="absolute overflow-hidden inset-0 bg-gray-200">
-      <LoginPage />
+      <AuthProvider>
+        <LoginPage />
+      </AuthProvider>
     </div>
   );
 }
