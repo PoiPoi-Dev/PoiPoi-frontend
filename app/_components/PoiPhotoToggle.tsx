76d92de9d9ef3dd740e0cb6abb1f15aa5299d87b
Jarrod Ariola <jarrod.ariola@gmail.com>
2024-06-16 11:32:46 +0900

    chore: improve useEffect code in PoiPhotoToggle since outcome was the same

--- app/_components/PoiPhotoToggle.tsx
+++ app/_components/PoiPhotoToggle.tsx
@@ -2,13 +2,11 @@ import { useState, useEffect } from "react";
 import { Pin } from "../_utils/global";
 import {
   Coordinates,
-  // GetDistanceFromCoordinatesToMeters,
   isCoordinatesWithinPinSearchZone,
 } from "../_utils/coordinateMath";
 import Image from "next/image";
 
 interface PoiPhotoToggleProps {
-  // pins: Pin[];
   userCoordinates: Coordinates | null;
   closestNotCompletedPin: Pin | null;
   showPopup: boolean;
@@ -17,7 +15,6 @@ interface PoiPhotoToggleProps {
 }
 
 const PoiPhotoToggle = ({
-  // pins,
   userCoordinates,
   closestNotCompletedPin,
   showPopup,
@@ -26,28 +23,17 @@ const PoiPhotoToggle = ({
 }: PoiPhotoToggleProps): React.JSX.Element => {
   // USE STATE
   const [pinToDisplay, setPinToDisplay] = useState<Pin | null>(closestNotCompletedPin);
-  // const [distanceToPin, setDistanceToPin] = useState<number>(0);
   const [isActiveState, setIsActiveState] = useState<boolean>(false);
 
   // USE EFFECT
-  // useEffect(() => {
-  //   const id = navigator.geolocation.watchPosition((position) => {
-  //     handleTrackingPinAndDistanceToPin(position.coords);
-  //   });
-  //   return () => navigator.geolocation.clearWatch(id);
-  // }, [pins, pinToDisplay?.is_completed]);
 
   useEffect(() => {
     setPinToDisplay(closestNotCompletedPin);
   },[closestNotCompletedPin])
 
   useEffect(() => {
-    if (!pinToDisplay) {
+    if (!pinToDisplay || !userCoordinates) {
       setIsActiveState(false);
-      return;
-    }
-    if (!userCoordinates) {
-      setIsActiveState(false)
       return;
     }
       setIsActiveState(isCoordinatesWithinPinSearchZone(userCoordinates, pinToDisplay));
@@ -73,43 +59,6 @@ const PoiPhotoToggle = ({
       </div>
     );
   };
-
-  // const handleTrackingPinAndDistanceToPin = (
-  //   userCoords: GeolocationCoordinates
-  // ) => {
-  //   const userCoordinates: Coordinates = {
-  //     longitude: userCoords.longitude,
-  //     latitude: userCoords.latitude,
-  //   };
-  //   let shortestDistance: number = Number.MAX_SAFE_INTEGER;
-  //   let pinToTrack: Pin | null = null;
-
-  //   // Finds the closest pin
-  //   for (const pin of pins) {
-  //     if (pin.is_completed) {
-  //       continue;
-  //     }
-  //     const pinCoordinates: Coordinates = {
-  //       longitude: pin.search_longitude,
-  //       latitude: pin.search_latitude,
-  //     };
-  //     const distance: number = GetDistanceFromCoordinatesToMeters(
-  //       userCoordinates,
-  //       pinCoordinates
-  //     );
-  //     if (distance < shortestDistance) {
-  //       shortestDistance = distance;
-  //       pinToTrack = pin;
-  //     }
-  //   }
-  //   setPinToDisplay(pinToTrack);
-  //   setDistanceToPin(shortestDistance);
-  // };
-
-  // const isWithinSearchZone = (): boolean => {
-  //   if (pinToDisplay) return distanceToPin < pinToDisplay.search_radius;
-  //   else return false;
-  // };
 
   // RETURN
   return <>{isActiveState ? handleRenderPhoto() : null}</>;
