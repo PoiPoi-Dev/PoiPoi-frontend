b7e23f0ba78638ffe3aa098c962827ed9a885a69
Dominik Sakic <dominik.sakic@yahoo.com>
2024-06-17 10:39:40 +0900

    chore: move revalidate to ssr component

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,8 +1,6 @@
 import { Suspense } from "react";
 import Loading from "../map/loading";
 import Leaderboard from "./Leaderboard";
-import { revalidatePath } from "next/cache";
-revalidatePath("/api/map/leaderboard");
 
 export default function Home(): JSX.Element {
   return (
