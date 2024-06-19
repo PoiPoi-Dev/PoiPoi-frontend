f9ce19a0ba3a7c3df9629c1934e040806f6af82c
Nattawat Pitikomon <nattawat.arch@gmail.com>
2024-06-19 11:07:27 +0900

    style: replace tutorial images and adjust the size and minor styling

--- app/how-to-play/HowToPlay.tsx
+++ app/how-to-play/HowToPlay.tsx
@@ -9,10 +9,23 @@ import {
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import Poipoi from "@/public/Poipoi.png";
+import P from "@/public/P.png";
 import { IoClose } from "react-icons/io5";
+import { IoWarning } from "react-icons/io5";
+import HTPstart from "@/public/HTP-Start.jpg";
+import HTPtrack1 from "@/public/HTP-TrackingPOI-1.jpg";
+import HTPtrack2 from "@/public/HTP-TrackingPOI-2.jpg";
+import HTPtrack3 from "@/public/HTP-TrackingPOI-3.jpg";
+import guess1 from "@/public/HTP-MakeAGuess-1.jpg";
+import guess2 from "@/public/HTP-MakeAGuess-2.jpg";
+import score from "@/public/HTP-ScoreAndLevel-1.jpg";
+import hint1 from "@/public/HTP-LeavingHint-1.jpg";
+import hint2 from "@/public/HTP-LeavingHint-2.jpg";
+import poidex1 from "@/public/HTP-POIdex-1.jpg";
+import poidex2 from "@/public/HTP-POIdex-2.jpg";
 
 const contentStyle =
-  "flex-col h-svh overflow-y-scroll no-scrollbar items-end justify-center text-left pb-8";
+  "flex flex-col w-full h-svh overflow-y-scroll no-scrollbar items-center justify-center text-left pb-8";
 
 export default function HowToPlay() {
   return (
@@ -29,24 +42,19 @@ export default function HowToPlay() {
             <Image
               alt="welcome screen"
               className="mt-4 rounded-2xl"
-              src="https://firebasestorage.googleapis.com/v0/b/tokyo-quest.appspot.com/o/devPhotos%2FIMG_3593.jpg?alt=media&token=47a552ab-701a-48e2-b1fb-e3dcaeab52a3"
-              width={400}
-              height={600}
+              src={P}
+              height={420}
             ></Image>
-            <h1 className="text-primary text-xl">WELCOME</h1>
+            <h1 className="text-primary text-xl mt-2">WELCOME</h1>
             <p className="mb-20">
               {`Welcome to POIPOI! POI stands for "Point of Interest", which is what you will be collecting throughout the game.`}
             </p>
           </CarouselItem>
           <CarouselItem className={contentStyle}>
-            <Image
-              alt="Tutorial 1"
-              className="mt-4 rounded-xl"
-              src="https://firebasestorage.googleapis.com/v0/b/tokyo-quest.appspot.com/o/devPhotos%2FIMG_3593.jpg?alt=media&token=47a552ab-701a-48e2-b1fb-e3dcaeab52a3"
-              width={400}
-              height={600}
-            ></Image>
-            <h2 className="text-primary text-xl">**BEFORE YOU PLAY**</h2>
+            <div className="flex justify-center items-center w-full py-8">
+              <IoWarning size={120} className="text-destructive" />
+            </div>
+            <h2 className="text-primary text-xl mt-2">**BEFORE YOU PLAY**</h2>
             <p className="mb-20">
               {`Please make sure to give permission to location tracking in your browser, as well as your phone!`}
               <br></br>
@@ -57,11 +65,10 @@ export default function HowToPlay() {
             <Image
               alt="Tutorial 2"
               className="mt-4 rounded-xl"
-              src="https://firebasestorage.googleapis.com/v0/b/tokyo-quest.appspot.com/o/devPhotos%2FIMG_3593.jpg?alt=media&token=47a552ab-701a-48e2-b1fb-e3dcaeab52a3"
-              width={400}
-              height={600}
+              src={HTPstart}
+              height={420}
             ></Image>
-            <h2 className="text-primary text-xl">HOW TO PLAY</h2>
+            <h2 className="text-primary text-xl mt-2">HOW TO PLAY</h2>
             <p className="mb-20">
               {`1. Select a question mark to start tracking your first POI.`}
               <br></br>
@@ -74,11 +81,12 @@ export default function HowToPlay() {
             <Image
               alt="Tutorial 3"
               className="mt-4 rounded-xl"
-              src="https://firebasestorage.googleapis.com/v0/b/tokyo-quest.appspot.com/o/devPhotos%2FIMG_3593.jpg?alt=media&token=47a552ab-701a-48e2-b1fb-e3dcaeab52a3"
-              width={400}
-              height={600}
+              src={HTPtrack1}
+              height={420}
             ></Image>
-            <h2 className="text-primary text-xl">Tracking a POI {`(1/3)`}</h2>
+            <h2 className="text-primary text-xl mt-2">
+              Tracking a POI {`(1/3)`}
+            </h2>
             <p className="mb-20">
               {`Each question mark on the map is a POI. The yellow circle around it indicates the "search zone", where hints and guessing will be activated.`}
             </p>
@@ -87,11 +95,12 @@ export default function HowToPlay() {
             <Image
               alt="Tutorial 4"
               className="mt-4 rounded-xl"
-              src="https://firebasestorage.googleapis.com/v0/b/tokyo-quest.appspot.com/o/devPhotos%2FIMG_3593.jpg?alt=media&token=47a552ab-701a-48e2-b1fb-e3dcaeab52a3"
-              width={400}
-              height={600}
+              src={HTPtrack2}
+              height={420}
             ></Image>
-            <h2 className="text-primary text-xl">Tracking a POI {`(2/3)`}</h2>
+            <h2 className="text-primary text-xl mt-2">
+              Tracking a POI {`(2/3)`}
+            </h2>
             <p className="mb-20">
               {`Clicking on any of the question marks will open the POI card and give you the option to track that POI, which is then visible at the top of the screen.`}
             </p>
@@ -100,11 +109,12 @@ export default function HowToPlay() {
             <Image
               alt="Tutorial 5"
               className="mt-4 rounded-xl"
-              src="https://firebasestorage.googleapis.com/v0/b/tokyo-quest.appspot.com/o/devPhotos%2FIMG_3593.jpg?alt=media&token=47a552ab-701a-48e2-b1fb-e3dcaeab52a3"
-              width={400}
-              height={600}
+              src={HTPtrack3}
+              height={420}
             ></Image>
-            <h2 className="text-primary text-xl">Tracking a POI {`(3/3)`}</h2>
+            <h2 className="text-primary text-xl mt-2">
+              Tracking a POI {`(3/3)`}
+            </h2>
             <p className="mb-20">
               {`If you are currently inside a search zone, the closest POI will be selected and tracked for you, and the photo will be displayed as a thumbnail in the bottom right corner. Clicking the thumbnail will also open the POI card.`}
             </p>
@@ -113,11 +123,12 @@ export default function HowToPlay() {
             <Image
               alt="Tutorial 6"
               className="mt-4 rounded-xl"
-              src="https://firebasestorage.googleapis.com/v0/b/tokyo-quest.appspot.com/o/devPhotos%2FIMG_3593.jpg?alt=media&token=47a552ab-701a-48e2-b1fb-e3dcaeab52a3"
-              width={400}
-              height={600}
+              src={guess1}
+              height={420}
             ></Image>
-            <h2 className="text-primary text-xl">Making a Guess {`(1/2)`}</h2>
+            <h2 className="text-primary text-xl mt-2">
+              Making a Guess {`(1/2)`}
+            </h2>
             <p className="mb-20">
               {`The goal of the game is to be as close to the location as possible.
            Remember, it is NOT the location of the object, but *WHERE THE PHOTO WAS TAKEN*! Once you enter a search zone, it will be possible to
@@ -128,11 +139,12 @@ export default function HowToPlay() {
             <Image
               alt="Tutorial 7"
               className="mt-4 rounded-xl"
-              src="https://firebasestorage.googleapis.com/v0/b/tokyo-quest.appspot.com/o/devPhotos%2FIMG_3593.jpg?alt=media&token=47a552ab-701a-48e2-b1fb-e3dcaeab52a3"
-              width={400}
-              height={600}
+              src={guess2}
+              height={420}
             ></Image>
-            <h2 className="text-primary text-xl">Making a Guess {`(2/2)`}</h2>
+            <h2 className="text-primary text-xl mt-2">
+              Making a Guess {`(2/2)`}
+            </h2>
             <p className="mb-20">
               {`Submitting a guess will take your current location and compare it to the POI's location, giving you the distance in meters and a score. It will also add the POI card to the Poidex.`}
             </p>
@@ -141,11 +153,12 @@ export default function HowToPlay() {
             <Image
               alt="Tutorial 8"
               className="mt-4 rounded-xl"
-              src="https://firebasestorage.googleapis.com/v0/b/tokyo-quest.appspot.com/o/devPhotos%2FIMG_3593.jpg?alt=media&token=47a552ab-701a-48e2-b1fb-e3dcaeab52a3"
-              width={400}
-              height={600}
+              src={score}
+              height={420}
             ></Image>
-            <h2 className="text-primary text-xl">Scoring and Leveling Up</h2>
+            <h2 className="text-primary text-xl mt-2">
+              Scoring and Leveling Up
+            </h2>
             <p className="mb-20">
               {`Scores are calculated based on the distance between your guess and the exact location of the POI. You can get as many as 1000 points per POI! Points also get added to the overall XP, which raises your level.`}
             </p>
@@ -154,11 +167,12 @@ export default function HowToPlay() {
             <Image
               alt="Tutorial 9"
               className="mt-4 rounded-xl"
-              src="https://firebasestorage.googleapis.com/v0/b/tokyo-quest.appspot.com/o/devPhotos%2FIMG_3593.jpg?alt=media&token=47a552ab-701a-48e2-b1fb-e3dcaeab52a3"
-              width={400}
-              height={600}
+              src={hint1}
+              height={420}
             ></Image>
-            <h2 className="text-primary text-xl">Leaving Hints {`(1/2)`}</h2>
+            <h2 className="text-primary text-xl mt-2">
+              Leaving Hints {`(1/2)`}
+            </h2>
             <p className="mb-20">
               {`If your guess is within 20 meters of the POI, you get the option to leave a hint! This hint can then be seen by others who have not yet collected the POI.`}
             </p>
@@ -167,11 +181,12 @@ export default function HowToPlay() {
             <Image
               alt="Tutorial 10"
               className="mt-4 rounded-xl"
-              src="https://firebasestorage.googleapis.com/v0/b/tokyo-quest.appspot.com/o/devPhotos%2FIMG_3593.jpg?alt=media&token=47a552ab-701a-48e2-b1fb-e3dcaeab52a3"
-              width={400}
-              height={600}
+              src={hint2}
+              height={420}
             ></Image>
-            <h2 className="text-primary text-xl">Leaving Hints {`(2/2)`}</h2>
+            <h2 className="text-primary text-xl mt-2">
+              Leaving Hints {`(2/2)`}
+            </h2>
             <p className="mb-20">
               {`Please leave a short comment that will help others find the POI! Try to make it easy to understand, but not giving away too much 😉.`}
             </p>
@@ -179,12 +194,11 @@ export default function HowToPlay() {
           <CarouselItem className={contentStyle}>
             <Image
               alt="Tutorial 11"
-              className="mt-4 rounded-2xl"
-              src="https://firebasestorage.googleapis.com/v0/b/tokyo-quest.appspot.com/o/devPhotos%2FIMG_3593.jpg?alt=media&token=47a552ab-701a-48e2-b1fb-e3dcaeab52a3"
-              width={400}
-              height={600}
+              className="mt-4 rounded-xl"
+              src={poidex1}
+              height={420}
             ></Image>
-            <h2 className="text-primary text-2xl">Poidex {`(1/2)`}</h2>
+            <h2 className="text-primary text-2xl mt-2">Poidex {`(1/2)`}</h2>
             <p className="mb-20">
               {`In the Poidex, you can see thumbnail photos of all the POIs and their current status. Clicking on a thumbnail will display the POI card. Clicking on the "navigate" icon in the top-right corner will take you to the search zone if the POI has not been collected.`}
             </p>
@@ -192,16 +206,15 @@ export default function HowToPlay() {
           <CarouselItem className={contentStyle}>
             <Image
               alt="Tutorial 12"
-              className="mt-4 rounded-2xl"
-              src="https://firebasestorage.googleapis.com/v0/b/tokyo-quest.appspot.com/o/devPhotos%2FIMG_3593.jpg?alt=media&token=47a552ab-701a-48e2-b1fb-e3dcaeab52a3"
-              width={400}
-              height={600}
+              className="mt-4 rounded-xl"
+              src={poidex2}
+              height={420}
             ></Image>
-            <h2 className="text-primary text-2xl">Poidex {`(2/2)`}</h2>
+            <h2 className="text-primary text-2xl mt-2">Poidex {`(2/2)`}</h2>
             <p>
               {`If the POI has been collected, the navigation icon will take you to the exact location of POI. You may revisit your favorite POIs anytime!`}
             </p>
-            <Link href="/map">
+            <Link href="/map" className="w-full">
               <Button className="w-full mt-4 mb-20">Return to Map</Button>
             </Link>
           </CarouselItem>
